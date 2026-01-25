12b90946a0ea9d076b03ed02c44d12f6f5ea5d05
宋业鑫 <syx0511@mail.ustc.edu.cn>
2026-01-25 04:05:48 +0000

    Add initialization of graph edges and constants for solving the problem

--- _posts/1.py
+++ _posts/1.py
@@ -2,6 +2,9 @@ import sys
 
 # 加速 IO
 input = sys.stdin.read
+
+inf = 10**9
+
 class Edge:
     def __init__(self, to, cost, cap):
         self.to = to
@@ -14,7 +17,13 @@ def solve():
     for _ in range(N):
         require.append(int(input()))
     nodes = [x for x in range(2*N+2)]
-    
+    edges = [[] for _ in range(2*N+2)]
+    # 建边
+    S = 0
+    T = 2*N+1
+    for i in range(1,N+1):
+        edges[S].append(Edge(i,p,inf))    
+        edges[i].append(Edge(T,0,require[i-1]))
 
 if __name__ == '__main__':
     solve()
