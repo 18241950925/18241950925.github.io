d6fa32e86dff32185d8e875ba262be67ce09de77
宋业鑫 <syx0511@mail.ustc.edu.cn>
2026-01-25 03:55:40 +0000

    Simplify solve function by removing unused dynamic programming logic and introducing Edge class for graph representation

--- _posts/1.py
+++ _posts/1.py
@@ -2,52 +2,19 @@ import sys
 
 # 加速 IO
 input = sys.stdin.read
+class Edge:
+    def __init__(self, to, cost, cap):
+        self.to = to
+        self.cost = cost
+        self.cap = cap
 
 def solve():
-    data = input().split()
-    if not data:
-        return
+    N,p,m,f,n,s = map(int, input().split())
+    require = []
+    for _ in range(N):
+        require.append(int(input()))
+    nodes = [x for x in range(2*N+2)]
     
-    n = int(data[0])
-    m = int(data[1])
-    
-    # 预处理 a，转为整数列表
-    a = []
-    idx = 2
-    for _ in range(n):
-        a.append(int(data[idx]))
-        idx += 1
-    
-    # dp 数组初始化，使用大数代替 1e15
-    INF = 10**15
-    dp = [0] * (n + 1)
-    
-    # dp[1] 单独处理或在循环中自然处理皆可，这里对应 C++ 的 dp[1] = a[0]
-    dp[1] = a[0]
-    
-    for i in range(2, n + 1):
-        cur = i
-        total = 0
-        current_max = a[i-1] 
-        min_temp = INF      
-        
-        while cur >= 1:
-            val = a[cur-1]
-            total += val
-            
-            if total > m:
-                break
-            if val > current_max:
-                current_max = val
-            cost = dp[cur-1] + current_max
-            if cost < min_temp:
-                min_temp = cost
-            
-            cur -= 1
-            
-        dp[i] = min_temp
-
-    print(dp[n])
 
 if __name__ == '__main__':
     solve()
